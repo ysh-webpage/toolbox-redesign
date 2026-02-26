a301eaf17db6f16d73d76eb2f28831dc5df055f2
ysh <mysh212@gmail.com>
2026-02-26 15:40:23 +0800

    Building table

--- src/pages/ncku_database.vue
+++ src/pages/ncku_database.vue
@@ -1,11 +1,15 @@
 <template>
     <layout>
-        <v-row>
-            <v-col>
+        <v-row justify = center>
+            <v-col cols = 8>
                 <v-text-field class = ma-3 v-model = search label = 尋 />
                 <v-card class = 'ma-3 text-center' title = 找人 prepend-icon = 'mdi-shield-search' v-ripple @click = submit />
             </v-col>
         </v-row>
+        <v-data-table-server
+            :headers = header
+            :items = data
+        />
     </layout>
 </template>
 
@@ -16,6 +20,7 @@ import { inject, ref, type Ref } from 'vue';
 
 const session: Ref<String> = inject('session')!;
 const logined: Ref<boolean> = inject('logined')!;
+const account: {[id: string]: Function} = inject('account')!;
 
 const info: Function = inject('info')!;
 const error: Function = inject('error')!;
@@ -24,6 +29,8 @@ const loading: Ref<boolean> = inject('loading')!;
 const search = ref('');
 const page = ref(0);
 const sort = ref('');
+const data = ref([]);
+const header: Ref<{[id: string]: string}[]> = ref([]);
 
 const submit = () => {
     loading.value = true;
@@ -39,6 +46,19 @@ const submit = () => {
         }
     }).done((response) => {
         response = JSON.parse(response);
+        if(!response.ok) {
+            error(response.error);
+            if(response.data?.logout) {
+                account.logout!();
+                location.href = '/';
+            }
+        }
+        data.value = response.data.result;
+        if(data.value.length > 0) header.value = Object.keys(data.value[0]!).map((x) => {return {
+            title: x,
+            key: x,
+            align: 'start'
+        }});
     }).fail(() => {
         error('請求失敗')
     }).always(() => {
